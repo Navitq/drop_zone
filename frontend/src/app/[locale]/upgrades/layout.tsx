c38353abc13f726b8f82269d53143440df64f313
navitq <zhenya.nikonov1999@gmail.com>
2025-08-11 21:42:06 +0300

    start to develope UpgradesComp

--- frontend/src/app/[locale]/upgrades/layout.tsx
+++ frontend/src/app/[locale]/upgrades/layout.tsx
@@ -1,4 +1,4 @@
-export default function FaqLayout({ children }) {
+export default function UpgradesLayout({ children }) {
     return (
         <>
             {children}
---
+++ frontend/src/app/[locale]/upgrades/page.tsx
@@ -0,0 +1,9 @@
+import style from '@/styles/upgrades.module.scss';
+
+export default function UpgradesPage() {
+    return (
+        <div className={style.upgrades}>
+            awd
+        </div >
+    );
+}
