6663024ba80a3fd1f293b6a59dc972fe8f50983e
navitq <zhenya.nikonov1999@gmail.com>
2025-08-19 22:09:49 +0300

    contracts desctop finished

--- frontend/src/components/CtStaff.tsx
+++ frontend/src/components/CtStaff.tsx
@@ -3,15 +3,20 @@ import React from 'react'
 import style from '@/styles/contracts.module.scss'
 import { useTranslations } from 'next-intl'
 
+import CtStaffSort from '@/components/CtStaffSort'
+import ExClientStuffs from '@/components/ExClientStuffs'
+
 function CtStaff(): React.ReactNode {
     const t = useTranslations("contracts")
     return (
         <div className={style.ctStaffCnt}>
             <div className={style.ctStaffTxtCnt}>
                 <div className={style.ctStaffTitle}>{t('accessible_objects')}</div>
-                <div></div>
+                <div className={style.ctStaffSortCnt}>
+                    <CtStaffSort></CtStaffSort>
+                </div>
             </div>
-
+            <ExClientStuffs ></ExClientStuffs>
         </div>
     )
 }
---
+++ frontend/src/components/CtStaffSort.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+import ExBlockTitle from '@/components/ExBlockTitle'
+import { useTranslations } from 'next-intl'
+
+import SortMenuAct from '@/components/SortMenuAct'
+
+import style from '@/styles/upgrades.module.scss'
+
+interface OptionType {
+    value: string,
+    label: string,
+};
+
+function CtStaffSort(): React.ReactNode {
+
+    const t = useTranslations("homePage");
+    const [options, setOptions] = useState<OptionType[] | null>(null);
+
+    useEffect(() => {
+        setOptions([
+            { value: 'srt_popularity', label: t('srt_popularity') },
+            { value: 'srt_price_up', label: t('srt_price_up') },
+            { value: 'srt_price_down', label: t('srt_price_down') },
+            { value: 'srt_name', label: t('srt_name') }
+        ]);
+    }, [t]);
+    return (
+        <div className={style.exClientCnt}>
+            {options ? <SortMenuAct options={options}></SortMenuAct> : null}
+        </div>
+    )
+}
+
+export default CtStaffSort
