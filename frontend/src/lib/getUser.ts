2b3a0f932a544d9c89e021fe0d36b09f9d445ef4
navitq <zhenya.nikonov1999@gmail.com>
2025-10-31 19:42:07 +0300

    docker for next prepared

--- frontend/src/lib/getUser.ts
+++ frontend/src/lib/getUser.ts
@@ -6,7 +6,7 @@ export async function getUser(cookieHeader?: string) {
   if (!cookieHeader) return null; // --- нет куки, сразу возвращаем null
 
   try {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}${BACKEND_PATHS.me}`, {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL || process.env.NEXT_DOCKER_API_BASE_URL}${BACKEND_PATHS.me}`, {
       headers: { cookie: cookieHeader },
       withCredentials: true,
     });
